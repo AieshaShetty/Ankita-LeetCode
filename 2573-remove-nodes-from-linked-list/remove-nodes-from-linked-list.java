181d847dec8cffdbcafe575f1eea41957fb8f283
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2024-05-06 23:31:41 +0530

    Time: 16 ms (44.20%) | Memory: 74.7 MB (24.62%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -10,29 +10,16 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Map<ListNode, Integer> maxMap = new HashMap<ListNode, Integer>();
-        setRightMax(head, maxMap);
-        ListNode dummyHead = new ListNode(0, head);
-        ListNode curr = dummyHead;
-        while (curr.next != null) {
-            if (maxMap.get(curr.next) > curr.next.val) {
-                curr.next = curr.next.next;
-            } else {
-                curr = curr.next;
-            }
-        }
-        return dummyHead.next;
-    }
+        if(head==null || head.next==null) return head;
 
-    private void setRightMax(ListNode node, Map<ListNode, Integer> maxMap) {
-        if (!maxMap.containsKey(node)) {
-            if (node.next != null) {
-                setRightMax(node.next, maxMap);
-                int maxRight = Math.max(node.next.val, maxMap.get(node.next));
-                maxMap.put(node, maxRight);
-            } else {
-                maxMap.put(node, -1);
-            }
+        ListNode  nextnode = removeNodes(head.next);
+
+        if(head.val < nextnode.val){
+            head=null;
+            return nextnode;
         }
+
+        head.next=nextnode;
+        return head;
     }
 }
