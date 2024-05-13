08e100e45dbf3d6caa7a5d2ca8c7f74bc84fb434
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2024-05-13 13:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (26.67%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.java
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -5,11 +5,19 @@ class Solution {
         int score = 0;
         for (int col = colCount - 1; col >= 0; col--) {
             int colSum = 0;
-            for (int row = rowCount - 1; row >= 0; row--) 
+            //bottom to top
+            for (int row = rowCount - 1; row >= 0; row--){
+                /*Count the number of matching bits in the column
+                 with the bit in column 0 of the row*/    
                 if (grid[row][col] == grid[row][0])
                     colSum++;
-            score += Math.max(colSum, rowCount - colSum) 
-                        << (colCount - col - 1);
+            }
+            
+            /* Choose the maximum between the column sum and 
+             the complement of the column sum, and add it to 
+             the total score shifted into the correct bit position
+             */
+            score += Math.max(colSum, rowCount - colSum) << (colCount - col - 1);
         }
         return score;
     }
