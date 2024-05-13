50ba3982f2560d1f89c5bb2f81e3c2f42ed355ff
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2024-05-14 00:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (62.71%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -1,5 +1,31 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
+        int[] result = new int[s.length()];
+            char[] chars = s.toCharArray();
+            for(int i = 0; i < chars.length; i++){
+
+                if(chars[i] == c){
+                    DFS(result, chars, c, i + 1, i);
+                    DFS(result, chars, c, i - 1, i);
+                }
+            }
+
+            return result;
+    }
+    private void DFS(int[] result, char[] chars, char c, int i, int pos) {
+            if(i < 0 || i >= result.length) return;
+            if(chars[i] == c) return;
+            int abs = Math.abs(pos - i);
+            if(result[i] != 0 && result[i] <= abs) return;
+
+            result[i] = abs;
+            DFS(result, chars, c, i + 1, pos);
+            DFS(result, chars, c, i - 1, pos);
+        }
+}
+
+/*class Solution {
+    public int[] shortestToChar(String s, char c) {
         List<CharacterIndex> letters = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
             letters.add(new CharacterIndex(i, s.charAt(i)));
@@ -35,3 +61,4 @@ class Solution {
         }
     }
 }
+*/
