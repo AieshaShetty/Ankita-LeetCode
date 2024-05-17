e1ef7fa93e6d2bd4946b333baf04689949ee1749
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2024-05-17 20:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (99.71%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public boolean isCompleteTree(TreeNode root) {
         // level order traversal
         
@@ -32,4 +32,42 @@ class Solution {
         return true;
     }
     
+}*/
+
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        // Get the total number of nodes in the tree
+        int totalNodes = countNodes(root);
+
+        // Check completeness using the helper function
+        return isComplete(root, 0, totalNodes);
+    }
+
+    // Function to count the total number of nodes
+    private int countNodes(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + countNodes(node.left) + countNodes(node.right);
+    }
+
+    // Function to check completeness by validating indices
+    private boolean isComplete(TreeNode node, int index, int totalNodes) {
+        if (node == null) {
+            return true;
+        }
+
+        // If the index is out of the expected range, the tree is not complete
+        if (index >= totalNodes) {
+            return false;
+        }
+
+        // Recursively check left and right subtrees
+        return isComplete(node.left, 2 * index + 1, totalNodes) &&
+               isComplete(node.right, 2 * index + 2, totalNodes);
+    }
 }
