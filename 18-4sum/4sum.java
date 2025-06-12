ead6d62b8b60ee5f2eb0daf8269c8c9c481e40c0
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2025-06-12 21:40:00 +0530

    Time: 20 ms (23.97%) | Memory: 43.9 MB (78.82%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,48 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        return kthSum(nums, target,0,4);
-    }
-	
-    public List<List<Integer>> kthSum(int[] nums, long target, int start, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        
-        if(start ==  nums.length) return res;
-        long average_value=target/k;
-        if  (nums[start] > average_value || average_value > nums[nums.length - 1]) {
-            return res;
-        }
-        
-        if(k==2) return twoSum(nums,target, start);
-    
-        for (int i = start; i < nums.length; ++i) {
-            if (i == start || nums[i - 1] != nums[i]) {
-                for (List<Integer> subset : kthSum(nums, target - nums[i], i + 1, k - 1)) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
-                    res.get(res.size() - 1).addAll(subset);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate j
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // skip duplicate left
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // skip duplicate right
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
                 }
             }
         }
-    
-        return res;
-    }
-	
-    public List<List<Integer>> twoSum(int[] nums, long target, int start) {
-        List<List<Integer>> res = new ArrayList<>();
-        int low = start, high = nums.length - 1;
-    
-        while (low < high) {
-            int currSum = nums[low] + nums[high];
-            if (currSum < target || (low > start && nums[low] == nums[low - 1])) {
-                ++low;
-            } else if (currSum > target || (high < nums.length - 1 && nums[high] == nums[high + 1])) {
-                --high;
-            } else {
-                res.add(Arrays.asList(nums[low++], nums[high--]));
-            }
-        }
-        
-                                                          
-        return res;
+
+        return result;
     }
 }
