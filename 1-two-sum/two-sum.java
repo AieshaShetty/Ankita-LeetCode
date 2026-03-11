923f60440e2046f8fb06cbd8de8bf88bf8406e25
Ankita Sahoo <79081320+AieshaShetty@users.noreply.github.com>
2026-03-12 00:31:38 +0530

    Time: 2 ms (99.22%) | Memory: 47.5 MB (8.87%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,33 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] original = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(nums);
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement= target-nums[i];
 
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-            if (sum == target) {
-                // Find the indices of the original unsorted array
-                int index1 = findIndex(original, nums[left], -1);
-                int index2 = findIndex(original, nums[right], index1);
-                return new int[] {index1, index2};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
+            map.put(nums[i],i);
         }
-        throw new IllegalArgumentException("No two sum solution");
-    }
-
-    private int findIndex(int[] nums, int value, int excludeIndex) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == value && i != excludeIndex) {
-                return i;
-            }
-        }
-        return -1;
+        return new int[]{};
     }
 }
